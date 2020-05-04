05f5fbb1c93542c86b865f24714a6002fe6a6fbd
Pierre Dechery <pierre.dechery@gmail.com>
2020-05-04 07:35:08 -0300

    Estilos

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'purecss/build/pure.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
